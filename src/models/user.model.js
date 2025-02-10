e6551367e005033422612f99b2f4f4982fe5f7ed
IqraRehmani <ahmadrehmani9206@gmail.com>
2025-02-10 23:21:37 +0500

    created user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,57 @@
+import mongoose , {Schema} from "mongoose"
+
+const userSchema = Schema(
+    {
+        username: {
+            type: String,
+            required : true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required:true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String,
+            required:true
+        },
+        coverImage: {
+            type: String,
+        },
+        watchHistory: [
+            {
+                type: Schema.types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+
+
+
+export const User = mongoose.model("User" , userSchema)
+
---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose , {Schema} from "mongoose"
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type:String,
+            required:true
+        },
+        thumbnail: {
+            type:String,
+            required: true
+        },
+        title: {
+            type:String,
+            required:true
+        },
+        description: {
+            type:String,
+            required:true
+        },
+        duration: {
+            type:Number,
+            required:true
+        },
+        views :{
+            type:Number,
+            default:0
+        },
+        isPublish: {
+            type: Boolean,
+            default:true
+        },
+        videoOwner:{
+            type:Schema.types.ObjectId,
+            ref:"User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Video = mongoose.model("Video", videoSchema)
