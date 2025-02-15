5595d11518a4277c84629a059156c11dd8e3231f
IqraRehmani <ahmadrehmani9206@gmail.com>
2025-02-15 23:19:31 +0500

    added routes

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static('public'))
 app.use(cookieParser)
+
+
+// import routes 
+
+
+
 export {app}
