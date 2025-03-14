c1cb56692a054c682c549d380a06a1b655fe0ceb
IqraRehmani <ahmadrehmani9206@gmail.com>
2025-03-15 00:01:54 +0500

    add some update controllers for user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -208,11 +208,55 @@ const refreshAccessToken = asyncHandler(async(req,  res)=>{
         )
     )
 })
+
+const changeCurrentPassword = asyncHandler(async(req, res)=>{
+    const {oldPassword, newPassword} = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "invalid password")
+    }
+    user.password = newPassword
+    await user.save({validateBeforeSave:false})
+
+    return res
+    .status(200)
+    .json( new ApiResponse(200, {}, "password changed successfully"))
+})
+
+const getCurrentUser = asyncHandler(async(req, res)=>{
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.user , "current user fetched successfully"))
+})
+
+const updateUserDetails = asyncHandler(async(req, res)=>{
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullname,
+                email
+            }
+        },
+        {new : true}
+    ).select("-password")
+
+    return res
+    .status(200),
+    json(new ApiResponse(200, user , "Account details updated successfully "))
+})
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserDetails
 }
 
 
